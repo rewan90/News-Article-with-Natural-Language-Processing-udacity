bf1adc7606b4e1526693021037954e71d1be92b0
Rewan Noufal <rewan.noufal.1998@gmail.com>
2024-09-25 19:41:49 +0300

    first commit

---
+++ src/client/index.js
@@ -0,0 +1,17 @@
+// js files
+import { handleSubmit } from './js/formHandler'
+import { checkForName } from './js/nameChecker'
+
+export { handleSubmit, checkForName };
+
+
+
+// alert("I EXIST")
+// console.log("CHANGE!!");
+
+// sass files
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
---
+++ src/server/index.js
@@ -0,0 +1,43 @@
+let path = require("path");
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+
+const cors = require("cors");
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static("dist"));
+console.log(__dirname);
+
+// Variables for url and api key
+let MeaningCloud = require('meaning-cloud');
+
+let meaning = MeaningCloud({
+    key: process.env.API_KEY, // API Key. Required.
+    secure: true   ,          // HTTPS or HTTPS. Optional, true by default.
+    uri: 'custom-uri',        // URI to create the API endpoints. Optional.
+  });
+
+console.log(`Your API key is ${process.env.API_KEY}`);
+
+app.get("/", function (req, res) {
+  res.send(
+    "This is the server API page, you may access its services via the client app."
+  );
+});
+app.get("/", function (req, res) {
+  res.sendFile("dist/index.html");
+});
+
+// POST Route
+
+// Designates what port the app will listen to for incoming requests
+app.listen(8000, function () {
+  console.log("Example app listening on port 8000!");
+});
